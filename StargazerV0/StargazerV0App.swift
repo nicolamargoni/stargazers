0ef5b87a8fb5dfa2d020aa47c780b8a6a8c26ea8
Nicola Margoni <margoni.nicola@gmail.com>
2021-10-25 16:51:32 +0200

    Initial Commit

---
+++ StargazerV0/StargazerV0App.swift
@@ -0,0 +1,17 @@
+//
+//  StargazerV0App.swift
+//  StargazerV0
+//
+//  Created by Nicola Margoni on 25/10/21.
+//
+
+import SwiftUI
+
+@main
+struct StargazerV0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StargazerV0Tests/StargazerV0Tests.swift
@@ -0,0 +1,33 @@
+//
+//  StargazerV0Tests.swift
+//  StargazerV0Tests
+//
+//  Created by Nicola Margoni on 25/10/21.
+//
+
+import XCTest
+@testable import StargazerV0
+
+class StargazerV0Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
